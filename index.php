bce28a751ccbaf26bfb33d65aa2952d17f3415ff
pratu-thorat <69445319+pratu-thorat@users.noreply.github.com>
2020-10-19 13:01:37 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php // Local IP Check
 $localIP = getHostByName(getHostName());?>
 <html>
-<head> Hello World ! Upadted PHP </head>
+<head> Hello  OpenshiftWorld !!! Upadted PHP </head>
   <h2> IP : <?php echo $localIP; ?></h2>
 </html>
